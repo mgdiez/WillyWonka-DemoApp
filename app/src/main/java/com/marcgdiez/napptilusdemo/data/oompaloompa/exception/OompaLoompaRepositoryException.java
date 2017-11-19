9bd7c6814632e9930c8b0b8a924d607f5f334728
Marc González Díez <marcgdiez@gmail.com>
2017-11-19 13:54:51 +0100

    Repository for Oompa Detail & Test

---
+++ app/src/main/java/com/marcgdiez/napptilusdemo/data/oompaloompa/exception/OompaLoompaRepositoryException.java
@@ -0,0 +1,9 @@
+package com.marcgdiez.napptilusdemo.data.oompaloompa.exception;
+
+public class OompaLoompaRepositoryException extends Exception {
+  private static final long serialVersionUID = 8810910569793396778L;
+
+  public OompaLoompaRepositoryException(String message) {
+    super(message);
+  }
+}
